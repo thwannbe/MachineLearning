2836725b75f89f2628f84e214116ee787cb286d9
thwannbe <suwon@csap.snu.ac.kr>
2014-09-22 15:30:45 +0900

    initialize concept

---
+++ concept_learning/concept.h
@@ -0,0 +1,13 @@
+/*************************************************************
+ * concept.h -- concept learning framework header file
+ *
+ * Author : Suwon, Oh -- Computer System And Platform Lab.,
+ *			 SNU, Korea
+ * 
+ * Purpose : Application trial for personal studying
+ *
+ * Last Update Date : Sep. 22. 2014
+ *
+ **************************************************************/
+
+ 
